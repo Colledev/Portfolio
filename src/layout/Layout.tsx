e1f3baa6d19a15c1c109fddcb6019f30acb96e46
Gabriel Colle <gabrielcolle3@hotmail.com>
2024-06-19 02:20:23 -0300

    feat: add Pages

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -8,7 +8,7 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
     return (
-        <div className="flex flex-col min-h-screen">
+        <div className="flex flex-col min-h-screen mx-20 sm:mx-32 md:mx-26 lg:mx-44 xl:mx-56 ">
             <Header />
             <main className="flex-1">{children}</main>
             <Footer />
